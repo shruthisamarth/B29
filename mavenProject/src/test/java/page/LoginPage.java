c6eecca18059ef86df3cc79a597f2173e96e2ae6
Shruthi <Shruthi@192.168.1.8>
2022-03-07 08:01:29 +0530

    Added Login Page

---
+++ mavenProject/src/test/java/page/LoginPage.java
@@ -0,0 +1,20 @@
+package page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+	@FindBy(id="username")
+	private WebElement unTB;
+	
+	public LoginPage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
+	
+	public void setUserName(String un) {
+		unTB.sendKeys(un);
+	}
+}
